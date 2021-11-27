3d35e279a00c3ed18e6e4d9b740a229dd55205bb
Crowdbotics <team@crowdbotics.com>
2021-11-27 20:21:37 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tdf_special_order_32179/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tdf_special_order_32179;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tdf_special_order_32179";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tdf_special_order_32179",
+  url: "https://tdf_special_order_32179.botics.co",
+  api: "https://tdf_special_order_32179.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
